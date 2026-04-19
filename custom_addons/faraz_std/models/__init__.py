b94be7e4766d2e32a4a7e0e7befe6829ab1a0422
Faraz Ahmed <faraz.ahmed333666999@gmail.com>
2026-04-19 18:52:16 +0500

    deleted custom_addons

--- custom_addons/faraz_std/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import student_model
-from . import school_model
-from . import course_model
--- custom_addons/uni_marksheet/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import student
-from . import university
-from . import course
-from . import subject_line
